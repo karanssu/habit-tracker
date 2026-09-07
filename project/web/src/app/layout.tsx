fe5bc2fe4d43df7546dc8b99f494b0b7e986bd24
karanssu <s0377009salemstate@gmail.com>
2026-09-07 18:49:32 -0400

    global.css warning fixed

--- project/web/src/app/layout.tsx
+++ project/web/src/app/layout.tsx
@@ -2,14 +2,18 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Habit Tracker",
-  description: "A small habit tracker to relearn Next.js with.",
+	title: "Habit Tracker",
+	description: "A small habit tracker to relearn Next.js with.",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body className="min-h-screen">{children}</body>
-    </html>
-  );
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en">
+			<body className="min-h-screen">{children}</body>
+		</html>
+	);
 }
